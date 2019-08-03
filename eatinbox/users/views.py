73a51fa599219379a65e7eb89a8a69ff8f0946dd
kulkarnisaumitra98 <kulkarnisaumitra98@gmail.com>
2019-08-03 19:01:05 +0530

    test successful

--- eatinbox/users/views.py
+++ eatinbox/users/views.py
@@ -3,8 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Create your views here.
-# This is a test comment
 
 
 def index(request):
-    return Response(status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_200_OK)
