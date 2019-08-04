6117fc831a352e53b736ce997d23bca379f16d66
deva777m <42939296+deva777m@users.noreply.github.com>
2019-08-04 13:03:08 +0530

    add Person, Vendor models

--- eatinbox/users/models.py
+++ eatinbox/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Person(models.Model):     # baseModel
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location_coordinates_X = models.DecimalField(max_digits=8, decimal_places=6, null=True)
+    location_coordinates_Y = models.DecimalField(max_digits=8, decimal_places=6, null=True)
+    contact = models.CharField(max_length=10, null=True, blank=True)
+
