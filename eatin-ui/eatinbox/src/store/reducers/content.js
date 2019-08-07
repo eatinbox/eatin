0ccc4bb2fa02540c70772511202bc3eeb3b00e3c
kulkarnisaumitra98 <kulkarnisaumitra98@gmail.com>
2019-08-07 22:04:24 +0530

    delete myenv shivang

--- eatin-ui/eatinbox/src/store/reducers/content.js
+++ eatin-ui/eatinbox/src/store/reducers/content.js
@@ -1,23 +1,23 @@
 const initialState = {
-    contentArray: [
-        {
-            url: require('../../assets/pic1.jpg'),
-            heading: 'Lorem Ipsum',
-            content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-        },
+    // contentArray: [
+    //     {
+    //         url: require('../../assets/pic1.jpg'),
+    //         heading: 'Lorem Ipsum',
+    //         content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
+    //     },
 
-        {
-            url: require('../../assets/pic2.jpg'),
-            heading: 'Lorem Ipsum',
-            content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-        },
+    //     {
+    //         url: require('../../assets/pic2.jpg'),
+    //         heading: 'Lorem Ipsum',
+    //         content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
+    //     },
 
-        {
-            url: require('../../assets/pic3.jpg'),
-            heading: 'Lorem Ipsum',
-            content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-        },
-    ]
+    //     {
+    //         url: require('../../assets/pic3.jpg'),
+    //         heading: 'Lorem Ipsum',
+    //         content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
+    //     },
+    // ]
 }
 
 const reducer = (state=initialState, action) => {
