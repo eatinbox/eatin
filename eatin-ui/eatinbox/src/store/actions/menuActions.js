bfea4f421aff6bbf46dbbed7017451212d46cced
kulkarnisaumitra98 <kulkarnisaumitra98@gmail.com>
2019-08-07 23:39:55 +0530

    add fetch user list action

--- eatin-ui/eatinbox/src/store/actions/menuActions.js
+++ eatin-ui/eatinbox/src/store/actions/menuActions.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const GET_MENU_LIST = 'GET_MENU_LIST'
 
 export const getMenuList = () => {
-    const url = 'http://192.168.0.105:8000/get-menu-list/'
+    const url = 'http://192.168.0.110:8000/get-user-list/'
 
     return async dispatch => {
         try {
