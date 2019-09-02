16a7cf101635e07f95d08547720fe05283fc16b3
deva777m <42939296+deva777m@users.noreply.github.com>
2019-09-02 17:33:00 +0530

    register user serializer & jwt

--- eatin-server/eatinbox/test.py
+++ eatin-server/eatinbox/test.py
@@ -3,16 +3,19 @@ import json
 import requests
 
 AUTH_ENDPOINT = "http://127.0.0.1:8000/auth/obtaintoken/"
+RETRIEVE_ENDPOINT = "http://127.0.0.1:8000/auth/register/14"
 
 headers ={
     "Content-Type": "application/json",
-    "Authorization": "JWT " + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6InNhdW1pIiwiZXhwIjoxNTY3OTMxMTg1LCJlbWFpbCI6IiIsIm9yaWdfaWF0IjoxNTY3MzI2Mzg1fQ.J6R7xxrdq_OgACiBL1O0p_kLRZ6ZZIbiGnrikCQUe3Y',
+    "Authorization": "JWT " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMywidXNlcm5hbWUiOiJWc2F1bWk2QGdtYWlsLmNvbSIsImV4cCI6MTU2NzQyNTg3MiwiZW1haWwiOiJWc2F1bWk2QGdtYWlsLmNvbSJ9.JGkGWvrclvS3JD73P8r_r8opVSwbHjinFuRV0-ZDF4A"
 }
-
 data = {
-    'username': 'saumi',
+    'username': 'saumitra@gmail.com',
     'password': '1'
 }
 
-r = requests.post(AUTH_ENDPOINT, data=json.dumps(data), headers=headers)
+#r = requests.post(AUTH_ENDPOINT, data=json.dumps(data), headers=headers)
+#print(r.json())
+
+r = requests.get(RETRIEVE_ENDPOINT, headers=headers)
 print(r.json())
