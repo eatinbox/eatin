e36d9dd7a8a4d198b5855aff897c3c4be6e9342a
kulkarnisaumitra98 <kulkarnisaumitra98@gmail.com>
2019-11-07 04:31:50 +0000

    init-pythonanywhere

--- eatin-server/eatinbox/eatinbox/settings.py
+++ eatin-server/eatinbox/eatinbox/settings.py
@@ -96,9 +96,10 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/etc/mysql/my.cnf',
-        },
+        'NAME': 'eatinbox$eatinbox',
+        'USER': 'eatinbox',
+        'PASSWORD': 'embrace98',
+        'HOST': 'eatinbox.mysql.pythonanywhere-services.com',
     }
 }
 
